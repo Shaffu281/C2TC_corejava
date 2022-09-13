bfd925a805f3b78e0bc8c95895de3d28aa43e5dd
QC <QC@DESKTOP-A1MK7K4>
2022-09-13 21:33:59 +0530

    First Commit

---
+++ C2TCjava/src/com/tns/prog/Oops_concepts.java
@@ -0,0 +1,28 @@
+package com.tns.prog;
+
+
+public class Oops_concepts {
+	int year_ofpurchase;
+	String make;
+	int model;
+	double cost;
+	public void display1()
+	{
+		System.out.println("yearof purs"+2001);
+		System.out.println("make"+"suzuki");
+		
+	}
+	void disp2()
+	{
+		System.out.println("model"+1999);
+		System.out.println("make"+500000);
+	}
+	 public static void main(String[] args) {
+		 // TODO Auto-generated method stub
+       Oops_concepts ob1 = new Oops_concepts();
+       
+       ob1.display1();
+       ob1.disp2();
+	}
+
+}
---
+++ C2TCjava/src/com/tns/prog/inheritance.java
@@ -0,0 +1,17 @@
+package com.tns.prog;
+class withdraw 
+{
+	int amt_withdraw=2000;
+}
+
+public class inheritance extends withdraw {
+	int amt_left=5000;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+inheritance ob1= new inheritance();
+System.out.println("amountwithdraw"+ ob1.amt_withdraw);
+	}
+
+}
