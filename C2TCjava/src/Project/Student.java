d79e744470e2077320a6c12fe94d6c38719c5def
QC <QC@DESKTOP-A1MK7K4>
2022-09-19 13:42:09 +0530

    First

--- C2TCjava/src/Project/Student.java
+++ C2TCjava/src/Project/Student.java
@@ -3,7 +3,6 @@ package Project;
 public class Student {
 	 Student(){
 		 System.out.println("The object of student is created");
-	 
 	 }
 		public static void main(String[] args) {
 			Student xc=new Student();
