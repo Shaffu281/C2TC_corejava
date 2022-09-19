65061da8ac0302d0a3ab10a87480762c37750f71
QC <QC@DESKTOP-A1MK7K4>
2022-09-19 13:15:52 +0530

    first

---
+++ C2TCjava/src/Project/Student.java
@@ -0,0 +1,12 @@
+package Project;
+
+public class Student {
+	 Student(){
+		 System.out.println("The object of student is created");
+	 
+	 }
+		public static void main(String[] args) {
+			Student xc=new Student();
+			// TODO Auto-generated method stub
+		}
+	}
---
+++ C2TCjava/src/Project/ThisKeyWord.java
@@ -0,0 +1,18 @@
+package Project;
+
+public class ThisKeyWord {
+	int c;
+	public ThisKeyWord(int c) {
+		this.c=c;
+		
+	}
+
+	public static void main(String[] args) {
+		ThisKeyWord vc=new ThisKeyWord(5);
+		System.out.println("the value of this c is ="+vc.c);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
