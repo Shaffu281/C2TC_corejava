3ef5857da06136d7b4c11f4fc5ada51a47181fad
QC <QC@DESKTOP-A1MK7K4>
2022-09-19 13:53:34 +0530

    First

--- C2TCjava/src/com/tns/assigment2/Student.java
+++ C2TCjava/src/com/tns/assigment2/Student.java
@@ -1,11 +1,12 @@
-package Project;
+package com.tns.assigment2;
+
 
 public class Student {
-	 Student(){
+	Student(){
 		 System.out.println("The object of student is created");
 	 }
 		public static void main(String[] args) {
 			Student xc=new Student();
-			// TODO Auto-generated method stub
-		}
 	}
+
+}
--- C2TCjava/src/com/tns/assigment2/ThisKeyword.java
+++ C2TCjava/src/com/tns/assigment2/ThisKeyword.java
@@ -1,12 +1,12 @@
-package Project;
+package com.tns.assigment2;
 
-public class ThisKeyWord {
+import Project.ThisKeyWord;
+
+public class ThisKeyword {
 	int c;
-	public ThisKeyWord(int c) {
+	public ThisKeyword(int c) {
 		this.c=c;
-		
 	}
-
 	public static void main(String[] args) {
 		ThisKeyWord vc=new ThisKeyWord(5);
 		System.out.println("the value of this c is ="+vc.c);
