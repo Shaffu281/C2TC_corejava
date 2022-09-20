bb2678d565e08b7007d23bdc7f28365b10965723
QC <QC@DESKTOP-A1MK7K4>
2022-09-20 21:49:08 +0530

    First

--- C2TCjava/src/com/tns/assigment2/ThisKeyword.java
+++ C2TCjava/src/com/tns/assigment2/ThisKeyword.java
@@ -1,13 +1,17 @@
 package com.tns.assigment2;
 
 public class ThisKeyword {
+
 	int c;
-	public ThisKeyword(int c) {
+	public  ThisKeyword(int c){
 		this.c=c;
 	}
+
 	public static void main(String[] args) {
+
 		ThisKeyword vc=new ThisKeyword(5);
 		System.out.println("the value of this c is ="+vc.c);
 	}
+
 
 }
