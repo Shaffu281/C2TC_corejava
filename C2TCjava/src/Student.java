7330a135db903f2ce942c60ec771a1a6e423a04d
QC <QC@DESKTOP-A1MK7K4>
2022-09-22 15:08:00 +0530

    commit

---
+++ C2TCjava/src/Student.java
@@ -0,0 +1,11 @@
+
+
+public class Student {
+	Student(){
+		 System.out.println("The object of student is created");
+	 }
+		public static void main(String[] args)
+		{
+			Student xc=new Student();
+	}
+}
