8f6608dfc5364902a3d1f000377544aa4f00636c
QC <QC@DESKTOP-A1MK7K4>
2022-09-22 21:12:08 +0530

    First Commit

---
+++ C2TCjava/src/com/tns/assignment/GreaterOrequal.java
@@ -0,0 +1,27 @@
+package com.tns.assignment;
+import java.util.Scanner;
+public class GreaterOrequal {
+
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		if(a>b) {
+			System.out.println("A is Bigger");
+		}else if(b>c){
+			System.out.println("b is Bigger");
+		}
+		else if(c>a){
+			System.out.println("c is Bigger");
+		}
+		else
+		{
+			System.out.println("All are Same");
+		}
+	
+
+	}
+
+}
