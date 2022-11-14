e3f84c8cce57870b929baceda0470a4a39af71af
Shaffu281 <113049292+Shaffu281@users.noreply.github.com>
2022-11-14 19:10:55 +0530

    first

---
+++ Springproject/src/main/java/com/tns/springproj/jio.java
@@ -0,0 +1,16 @@
+package com.tns.springproj;
+
+public class jio implements sim {
+		public jio() {
+			System.out.println("constructor");
+		}
+		
+	public void calling() {
+		System.out.println("jio call");
+	}
+	public void data() {
+		System.out.println("jio data");
+		
+		
+	}
+	}
---
+++ Springproject/src/main/java/com/tns/springproj/sim.java
@@ -0,0 +1,6 @@
+package com.tns.springproj;
+
+public interface sim {
+	void calling();
+	void data();
+}
